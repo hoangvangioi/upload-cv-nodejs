27ee3539a1e9a3c06db7210195e12c15e5c3e0af
Hoàng Văn Giỏi <gioitube>
2022-07-01 18:17:25 +0700

    deploy heroku

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const app = require('./app')
-require('dotenv').config()
-const port = process.env.PORT || 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const hostname = process.env.HOSTNAME
+const port = process.env.PORT || 8000;
+require('dotenv').config()
+
+const corsOptions = {
+    origin: [process.env.ORIGIN],
+}
+  
+global.__basedir = __dirname;
+
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./router");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server is running on ${hostname}:${port} .......`);
