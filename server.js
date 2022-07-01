5ededbda31f970e0c7219f50735f4312d2a3b27c
Hoàng Văn Giỏi <gioitube>
2022-07-01 16:57:35 +0700

    deploy netlify

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
+const app = require('./app')
 require('dotenv').config()
 const port = process.env.PORT || 8000;
 const hostname = process.env.HOSTNAME
-
-const corsOptions = {
-  origin: [process.env.ORIGIN],
-}
-
-global.__basedir = __dirname;
-
-
-app.use(express.static(`${__dirname}/public`));
-app.use(express.json());
-
-app.use(cors(corsOptions));
-
-const initRoutes = require("./src/router");
-
-app.use(express.urlencoded({ extended: true }));
-initRoutes(app);
-
 
 app.listen(port, () => {
   console.log(`Server is running on ${hostname}:${port} .......`);
